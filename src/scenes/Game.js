0dbf901ce5743bc249b02e6e428f4b470a857705
Raf <raf.dimartino@wardour.co.uk>
2022-03-07 11:08:07 +0000

    tutorial 2

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -17,11 +17,28 @@ class Game extends Phaser.Scene
 
         ball.body.setVelocity(-200, 0)
 
-        const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff)
-        this.physics.add.existing(paddleLeft, true) // true is setting a static body
-        paddleLeft.body
+        this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff) //create a rectangle
+        this.physics.add.existing(this.paddleLeft, true) // true is setting a static body
 
-        this.physics.add.collider(paddleLeft, ball) 
+        this.physics.add.collider(this.paddleLeft, ball) // set collider between ball and paddle 
+
+        this.cursors = this.input.keyboard.createCursorKeys() //create and return an object containing 4 hotkeys for up, down, let and right, spacebar and shift
+    }
+    update()
+    {
+        /** @type {Phaser.Physics.Arcade.StaticBody} */ // for better intellisense
+        const body = this.paddleLeft.body
+
+        if (this.cursors.up.isDown)
+        {
+            this.paddleLeft.y -= 10
+            body.updateFromGameObject()
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.paddleLeft.y += 10
+            body.updateFromGameObject()
+        }
     }
 }
 
