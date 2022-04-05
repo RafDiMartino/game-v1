1c120207b6e625e1fb6ead2f3bb86f99eb1c829c
Raf Di Martino <rafdimartino@war-mac20.local>
2022-04-05 10:12:01 +0100

    tutorial 6 background

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Phaser from 'phaser'
 import TitleScreen from './scenes/TitleScreen'
 import Game from './scenes/Game'
+import GameBackground from './scenes/GameBackground'
 
 const config = {
     width: 800,
     height: 500,
     type: Phaser.AUTO,
-    backgroundColor: "#616161",
+    //backgroundColor: "#616161",
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,6 +21,7 @@ const game = new Phaser.Game(config)
 
 game.scene.add('titlescreen', TitleScreen)
 game.scene.add('scene1', Game)
+game.scene.add('game-background', GameBackground)
 
 //game.scene.start('titlescreen')
 game.scene.start('scene1')
